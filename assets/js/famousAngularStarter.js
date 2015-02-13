fd7f842e482790387d176a6429edb5fb4f46bde0
Chris Kim <cj3kim@gmail.com>
2015-02-13 15:00:12 -0800

    Got famous-angular to work with SailsJS

---
+++ assets/js/famousAngularStarter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('famousAngularStarter',
+  ['ui.router',
+    'famous.angular' ])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+    $urlRouterProvider.otherwise('/');
+  })
+;
